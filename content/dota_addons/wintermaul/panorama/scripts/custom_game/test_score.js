f7902a4d46cde1891aa3d6dc57daad1968d389af
Sumaris <alfred.radu@gmail.com>
2018-01-20 18:00:27 +0100

    suck it valve

--- content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
+++ content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
@@ -8,8 +8,16 @@ function ScoreAdd( color, id){
 	panel.BLoadLayoutSnippet("Player");
 }
 
+var clicked = false 
 function BoardVisibility(){
 	//Slide in or out
+	if (clicked == false){
+		$.GetContextPanel().style.transform = "translatex( 300px)";
+		clicked = true;
+	} else {
+		$.GetContextPanel().style.transform = "translatex( 0px)";
+		clicked = false;
+	}
 }
 
 function debug(){
