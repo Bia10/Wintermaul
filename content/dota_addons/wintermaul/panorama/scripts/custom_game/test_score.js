617cd9297369e8bb1ed5ec5a01f9fc356ce807e6
Sumaris <alfred.radu@gmail.com>
2018-01-22 04:00:47 +0100

    scorescreen prototype up and running.

--- content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
+++ content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
@@ -1,29 +1,48 @@
-function ScoreUpdate(args){
+function ScoreUpdate( args){
 	//Update info for everyone about kills
+	$.Msg("Updating scoreboard:", Players.GetPlayerName(args.id), ": ", Players.GetLastHits(args.id));
+	
+	$("#"+String(args.id)).FindChildTraverse('PlayerKills').text = Players.GetLastHits(args.id);
+
 }
 
-function ScoreAdd( color, id){
+function ScoreAdd( id ){
 	//add new players to list
-	var panel = $.CreatePanel('Panel', $('#Players'), '');
-	panel.BLoadLayoutSnippet("Player");
+	var panel = $.CreatePanel('Panel', $('#ScoreBoard'), String(id.id));
+
+	panel.BLoadLayout("file://{resources}/layout/custom_game/score_child.xml",false,false);
+	$.Msg(id.id)
+	$.Msg("adding ", Players.GetPlayerName(1), ", with color ", Players.GetPlayerColor(id.id), " to scorelist.");
+
+	panel.FindChildTraverse('PlayerName').text = Players.GetPlayerName(id.id);
+	//panel.FindChildTraverse('PlayerName').style.color = "#"+Players.GetPlayerColor(id.id); //the fuck is up with this
+	panel.FindChildTraverse('PlayerKills').text = Players.GetLastHits(id.id);
+
+	panel.name = Players.GetPlayerName(id.id);
+	return panel;
 }
 
-var clicked = false 
-function BoardVisibility(){
-	//Slide in or out
-	if (clicked == false){
-		$.GetContextPanel().style.transform = "translatex( 300px)";
-		clicked = true;
-	} else {
-		$.GetContextPanel().style.transform = "translatex( 0px)";
-		clicked = false;
-	}
-}
+GameEvents.Subscribe( "new_player", ScoreAdd);
+GameEvents.Subscribe( "new_score", ScoreUpdate);
 
 function debug(){
 	$.Msg("Debug!"); 
-	ScoreAdd();
 }
 
 debug(); 
 //$.GetContextPanel().SetHasClass("Player", false);
+
+var clicked = false 
+function BoardVisibility(){
+	//Slide in or out
+	var btnbox = $("#btnText");
+	if (clicked == false){
+		$.GetContextPanel().style.transform = "translatex( 300px)";
+		clicked = true;
+		btnbox.text = "<";
+	} else {
+		$.GetContextPanel().style.transform = "translatex( 0px)";
+		clicked = false;
+		btnbox.text = ">";
+	}
+}
