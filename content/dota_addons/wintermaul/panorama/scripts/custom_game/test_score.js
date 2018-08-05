621d1d77cd5e238700fc94716d72aff24bc508a6
Ekin Inceleme <einceleme@gmail.com>
2018-08-05 16:14:39 +0100

    Hoping this works for player reconnect

--- content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
+++ content/dota_addons/wintermaul/panorama/scripts/custom_game/test_score.js
@@ -1,42 +1,63 @@
-function ScoreUpdate( args){
-	//Update info for everyone about kills
-	$.Msg("Updating scoreboard:", Players.GetPlayerName(args.id), ": ", Players.GetLastHits(args.id));
-	
-	$("#"+String(args.id)).FindChildTraverse('PlayerKills').text = Players.GetLastHits(args.id);
+function InitScoreboard() {
+    var player_ids = Game.GetAllPlayerIDs();
+    
+    for (var i = 0; i < player_ids.length; i++) {
+        ScoreAdd(player_ids[i])
+    }
+    ScoreUpdate()
+}
+
+function ScoreAdd(id) {
+    //add new players to list
+    var panel = $.CreatePanel('Panel', $('#ScoreBoard'), String(id));
+    panel.BLoadLayout("file://{resources}/layout/custom_game/score_child.xml", false, false);
+    return panel;
+}
+
+function ScoreUpdate() {
+    var player_stats = CustomNetTables.GetAllTableValues("player_scores");
+    var player_ids = Game.GetAllPlayerIDs();
+    for (var player_id in player_ids) {
+        int_player_id = parseInt(player_id);
+        player_stats.push({
+            id: player_id,
+            name: Players.GetPlayerName(int_player_id),
+            score: Players.GetLastHits(int_player_id)
+        })
+    }
+
+    player_stats.sort(function (obj1, obj2) {
+        // Descending: second age less than the first
+        return obj2.score - obj1.score;
+    });
+    $.Msg(player_stats)
+    
+    for (var key in player_stats) {
+        player = player_stats[key]
+        $("#" + player.id).FindChildTraverse('PlayerName').text = player.name;
+        $("#" + player.id).FindChildTraverse('PlayerKills').text = player.score;
+    }
+}
+
+var clicked = false
+
+function BoardVisibility() {
+    //Slide in or out
+    var btnbox = $("#btnText");
+    if (clicked == false) {
+        $.GetContextPanel().style.transform = "translatex( 300px)";
+        clicked = true;
+        btnbox.text = "<";
+    } else {
+        $.GetContextPanel().style.transform = "translatex( 0px)";
+        clicked = false;
+        btnbox.text = ">";
+    }
 
 }
 
-function ScoreAdd( id ){
-	//add new players to list
-	var panel = $.CreatePanel('Panel', $('#ScoreBoard'), String(id.id));
+(function () {
+    InitScoreboard()
+})();
 
-	panel.BLoadLayout("file://{resources}/layout/custom_game/score_child.xml",false,false);
-	$.Msg(id.id)
-	$.Msg("adding ", Players.GetPlayerName(1), ", with color ", GameUI.CustomUIConfig().player_colors[ id.id ], " to scorelist.");
-
-	panel.FindChildTraverse('PlayerName').text = Players.GetPlayerName(id.id);
-	panel.FindChildTraverse('PlayerName').style.color = GameUI.CustomUIConfig().player_colors[ id.id ];
-	panel.FindChildTraverse('PlayerKills').text = Players.GetLastHits(id.id);
-
-	panel.name = Players.GetPlayerName(id.id);
-
-	return panel;
-}
-
-GameEvents.Subscribe( "new_player", ScoreAdd);
-GameEvents.Subscribe( "new_score", ScoreUpdate); 
-
-var clicked = false 
-function BoardVisibility(){
-	//Slide in or out
-	var btnbox = $("#btnText");
-	if (clicked == false){
-		$.GetContextPanel().style.transform = "translatex( 300px)";
-		clicked = true;
-		btnbox.text = "<";
-	} else {
-		$.GetContextPanel().style.transform = "translatex( 0px)";
-		clicked = false;
-		btnbox.text = ">";
-	}
-}
+GameEvents.Subscribe("new_score", ScoreUpdate);
