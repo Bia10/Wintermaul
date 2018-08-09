9b2f7a48c60624d72f1d567f1fa0621305e7981f
sumaris <alfred.radu@gmai.com>
2018-08-09 16:41:04 +0200

    added sounds playing on spawn and fixed locating of towers

--- game/dota_addons/wintermaul/scripts/vscripts/bosses/ai_chieftain.lua
+++ game/dota_addons/wintermaul/scripts/vscripts/bosses/ai_chieftain.lua
@@ -1,7 +1,8 @@
 function Spawn( entityKeyValues )
-	print("Spawned a big mean boss!")
 	ABILITY_stun = thisEntity:FindAbilityByName( "chieftain_stomp" )
 	math.randomseed(1234)
+	EmitGlobalSound("Hero_ElderTitan.EchoStomp")
+	EmitGlobalSound("elder_titan_elder_levelup_15")
 	thisEntity:SetContextThink( "ChieftainThink", ChieftainThink, 14 )
 end
 
@@ -9,12 +10,22 @@ function ChieftainThink()
 	if thisEntity:IsNull() or not thisEntity:IsAlive() then
 		return nil -- deactivate this think function
 	end
+
 	t = math.random(14, 20)
-	local targets = FindUnitsInRadius(DOTA_TEAM_BADGUYS, thisEntity:GetAbsOrigin(), nil, 300, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
-	for k,v in pairs(targets) do print(k,v) end
+	local radius = ABILITY_stun:GetSpecialValueFor("%stomp_radius")
+	local targets = FindUnitsInRadius(
+		thisEntity:GetTeam(), 
+		thisEntity:GetAbsOrigin(), 
+		nil, 
+		100,
+		DOTA_UNIT_TARGET_TEAM_ENEMY, 
+		DOTA_UNIT_TARGET_ALL, 
+		DOTA_UNIT_TARGET_FLAG_NONE,
+		FIND_ANY_ORDER, 
+		false)
+
 	if #targets > 0 then
 		thisEntity:CastAbilityNoTarget(ABILITY_stun, -1)
-		print("Stomping")
 		return t
 	end
 	return t 
