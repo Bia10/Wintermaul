217983d8fc9fbae638f5d761c50c2910ecce6895
Ekin Inceleme <einceleme@gmail.com>
2018-09-01 22:13:17 +0100

    Forgot to add this file to last upgrade commit

--- game/dota_addons/wintermaul/scripts/vscripts/towers/upgrade.lua
+++ game/dota_addons/wintermaul/scripts/vscripts/towers/upgrade.lua
@@ -1,10 +1,10 @@
 -- called when a sell_tower_x ability is cast
 require('libraries/buildinghelper')
 function UpgradeTower(keys)
-    print('is thing on')
     local building = keys.caster
     local newBuildingName = keys.new_tower_name
     if building:GetHealth() == building:GetMaxHealth() then -- only allow selling if the building is fully built
-        BuildingHelper:UpgradeBuilding(building, newBuildingName)
+        local newBuilding = BuildingHelper:UpgradeBuilding(building, newBuildingName)
+        newBuilding.gold_cost = GameRules.AbilityKV[keys.ability:GetAbilityName()].AbilityGoldCost + building.gold_cost
     end
 end
